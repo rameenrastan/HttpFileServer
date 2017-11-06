4a953e4bd5dbaaffba6797dfcd5e46919d376fdb
Unknown <vincentfugnitto94@gmail.com>
2017-11-05 21:12:36 -0500

    Enhanced Server Code ( added print messages, error handling )

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ import threading
 # define directory name
 directory = "server-files"
 
-def server(host, port):
+
+# run web-server
+def run_server(host="localhost", port=8080):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         print("web-server socket initiated...")
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # host = socket.gethostbyname(host)
         listener.bind((host, port))
         listener.listen(10)
         print("web-server listening ( host: %s, port: %s ) " % (host, port))
@@ -18,10 +21,14 @@ def server(host, port):
             print("web-server is listening for incoming connection...")
             conn, addr = listener.accept()
             threading.Thread(target=handle_client, args=(conn, addr)).start()
+    except Exception as e:
+        print("web-server error occured: %s" % (e))
     finally:
+        print("web-server closing...")
         listener.close()
 
 
+# server handle user requests
 def handle_client(conn, addr):
     try:
         while True:
@@ -33,11 +40,10 @@ def handle_client(conn, addr):
         conn.close()
 
 
-
 def main():
-    server('',8080)
-    list_files()
-    read_file("test.txt")
+    run_server()
+    # list_files()
+    # read_file("test.txt")
 
 
 if __name__ == "__main__":
