04204a05726eb13f836587f3b0e1b53c9af6eba3
Unknown <vincentfugnitto94@gmail.com>
2017-11-06 12:59:07 -0500

    Minor updates to handling and list file formatting

--- test_client.py
+++ test_client.py
@@ -1,18 +1,23 @@
 # simple client to test sending requests to server-socket
 
 import socket
+import sys
 
 def client():
     # create client socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 9999))
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost", 9999))
+    except Exception as e:
+        print(e)
+        sys.exit()
 
     # handle user input to request from server
     clients_input = input("Please enter a request...\n")
     sock.send(clients_input.encode("utf8")) # we must encode the string to bytes
     server_response = sock.recv(4096).decode("utf8")
 
-    print("Httpfs server response: {}".format(server_response))
+    print("Server Response: \n %s" % server_response)
 
 
 def main():
